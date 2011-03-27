9150a7bceb111b3d3850c74d33389ef3baacee83
Kiran_నాని <kiran@Nanis-MacBook.local>
2011-03-28 00:18:11 +0530

    Initial Commit

---
+++ UniMarble/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UniMarble
+//
+//  Created by Kiran_నాని on 28-03-11.
+//  Copyright (c) 2011 ipap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ UniMarble/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UniMarble
+//
+//  Created by Kiran_నాని on 28-03-11.
+//  Copyright (c) 2011 ipap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
