de1c3a3f1798a1c7dafebde6332549bc681eb50e
Kiran_నాని <kiran@Nanis-MacBook.local>
2011-04-25 14:07:27 +0530

    until core complete.

--- UniMarble/ViewController.h
+++ UniMarble/ViewController.h
@@ -7,11 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "Holes.h"
 
 @interface ViewController : UIViewController
 {
-    NSArray *holeBounds;
+    NSArray *holeNumber;
 }
 
 
--- UniMarble/ViewController.m
+++ UniMarble/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    int betweenTag;
+    int FromTag;
+    int ToTag;
+}
 
 @end
 
@@ -18,8 +23,8 @@
 {
     [super viewDidLoad];
     
-    holeBounds = [[NSArray alloc]initWithObjects:
-                        @"2",@"3",@"4",
+    holeNumber = [[NSArray alloc]initWithObjects:
+                        @"2", @"3", @"4",
                         @"12",@"13",@"14",
             @"20",@"21",@"22",@"23",@"24",@"25",@"26",
             @"30",@"31",@"32",@"33",@"34",@"35",@"36",
@@ -28,88 +33,186 @@
                         @"62",@"63",@"64", nil];
     
     //tap gesture for holes
-    for (Holes *holes in self.view.subviews)
+    for (UIImageView *holes in self.view.subviews)
     {
-        if (holes.tag >= 2 && holes.tag <= 64) {
+        if (holes.tag >= 2 && holes.tag <= 64)
+        {
+            
+            // TapGesture for Holes
+            UITapGestureRecognizer *tapForHole = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleTapForHole:)];
+            [holes addGestureRecognizer:tapForHole];
+            [tapForHole release];
+            
+            
+            
+            
+            // creating a marble and making subview to hole.
+            if (holes.tag != 33)
+            {
+            UIImageView *marble = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 44, 44)];
+            marble.image = [UIImage imageNamed:@"Marble.png"];
+            marble.userInteractionEnabled = YES;
             UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleTap:)];
-            [holes addGestureRecognizer:tap];
+            [marble addGestureRecognizer:tap];
+            [holes addSubview:marble];
             [tap release];
             
-            // making all holes with objects
-            holes.containsBall = YES;
-           
-          //  NSLog(@"%d -> %d",holes.tag,holes.containsBall);
+            }
         }
     }
-    // making middle hole with no object
-    ((Holes *)[self.view viewWithTag:33]).containsBall = NO;
-    
-    //pan gesture for balls
-    for (UIImageView *balls in self.view.subviews)
-    {
-        if (balls.tag >= 111 && balls.tag <= 142) {
-            UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(handlePan:)];
-            [balls addGestureRecognizer:pan];
-            [pan release];
-        }
-    }	
 }
+
+
+
+#pragma mark - handles Holes
+
+-(void) handleTapForHole:(UITapGestureRecognizer *) sender
+{
+    NSLog(@"backGround:%@",((UIImageView *)sender.view).backgroundColor);
+    if ( ((UIImageView *)sender.view).backgroundColor != nil ) {
+              NSLog(@"%d -> no of balls:%d",FromTag,[[[self.view viewWithTag:FromTag] subviews] count]);
+        
+        
+  //     ((UIImageView *) [[[self.view viewWithTag:FromTag] subviews] objectAtIndex:0]).backgroundColor = [UIColor blueColor];
+        // remove marble from superview
+        
+        UIImageView *buffer =  [[[[self.view viewWithTag:FromTag] subviews] objectAtIndex:0] retain];
+        
+
+        [((UIImageView *)[[[self.view viewWithTag:FromTag] subviews] objectAtIndex:0]) removeFromSuperview];
+        [(UIImageView *)sender.view addSubview:buffer];
+    
+        int between = ((UIImageView *)sender.view).tag - FromTag;
+        NSLog(@"bet:%d",between);
+        switch (between) {
+            case -2:
+                betweenTag = ((UIImageView *)sender.view).tag + 1;
+                break;
+            case 2:
+                betweenTag = ((UIImageView *)sender.view).tag - 1;
+                break;
+            case -20:
+                betweenTag = ((UIImageView *)sender.view).tag + 10;
+                break;
+            case 20:
+                betweenTag = ((UIImageView *)sender.view).tag - 10;
+                break;
+            default:
+                break;
+        }
+        
+        
+        [((UIImageView *)[[[self.view viewWithTag:betweenTag] subviews] objectAtIndex:0]) removeFromSuperview];
+        
+        for (UIImageView *holes in self.view.subviews)
+        {
+            if (holes.tag >= 2 && holes.tag <= 64)
+            {
+                holes.backgroundColor = nil;
+            }
+        }
+
+        // add to sender view
+        
+        
+    }
+}
+
+
+
+#pragma mark - handles Marble
+
 
 -(void) handleTap:(UITapGestureRecognizer *) sender
 {
-        NSLog(@"-------tap---------");
-    int tag = ((Holes *)sender.view).tag;
     
-    if ([self containsBall:tag at:@"right"]) {
-        NSLog(@"glow right");
+    // clearing all glowing holes
+    for (UIImageView *holes in self.view.subviews)
+    {
+        if (holes.tag >= 2 && holes.tag <= 64)
+        {
+            holes.backgroundColor = nil;
+        }
     }
-    if ([self containsBall:tag at:@"left"]) {
-        NSLog(@"glow left");
-    }
-    if ([self containsBall:tag at:@"up"]) {
-        NSLog(@"glow up");
-    }
-    if ([self containsBall:tag at:@"down"]) {
-        NSLog(@"glow down");
-    }
+    
+    NSLog(@"___________%@_____________",NSStringFromSelector(_cmd));
+    int tag = [sender.view superview].tag;
+    NSLog(@"tag:%d",tag);
+    
+    [self containsMarble:tag at:@"right"];
+    [self containsMarble:tag at:@"left"];
+    [self containsMarble:tag at:@"up"];
+    [self containsMarble:tag at:@"down"];
+    
+//    
+//    if ([self containsMarble:tag at:@"right"]) {
+//        NSLog(@"glow right hole");
+//    }
+//    if ([self containsMarble:tag at:@"left"]) {
+//        NSLog(@"glow left hole");
+//    }
+//    if ([self containsMarble:tag at:@"up"]) {
+//        NSLog(@"glow up hole");
+//    }
+//    if ([self containsMarble:tag at:@"down"]) {
+//        NSLog(@"glow down hole");
+   // }
 }
 
 
--(BOOL) containsBall:(int)tag at:(NSString *)direction
+
+-(void) containsMarble:(int)tag at:(NSString *)direction
 {
-    int nextTag = [self requiredTag:tag inDirection:direction];
-    if ([self checkingBall:nextTag]) {
-        return [self checkingBall:[self requiredTag:nextTag inDirection:direction]];
+    FromTag = tag;
+    // going to the required direcion and return YES if hole contains marble
+    betweenTag = [self requiredTag:FromTag inDirection:direction];
+    if ([self isValidHole:betweenTag]){
+    // checking for first level
+    if ([self checkingForMarble:betweenTag]) {
+        // checking for second level
+        ToTag = [self requiredTag:betweenTag inDirection:direction];
+        if ([self isValidHole:ToTag]){
+            if (![self checkingForMarble:ToTag])
+            {
+            [self.view viewWithTag:ToTag].backgroundColor = [UIColor brownColor];
+        
+       // return [self checkingForMarble:[self requiredTag:nextTag inDirection:direction]];
+        }
+        }
     }
-    return  NO;
-}
-
-
--(void) handlePan:(UIPanGestureRecognizer *) sender
-{
-    
-    //[sender.view bringSubviewToFront:self.view];
-
-    CGPoint translation  = [sender translationInView:sender.view];
-    sender.view.center = CGPointMake(sender.view.center.x + translation.x, sender.view.center.y + translation.y);
-    [sender setTranslation:CGPointMake(0,0) inView:sender.view];
-    
-}
-
-
--(BOOL) checkingBall:(int)tag
-{
-    if ([self isValidHole:tag]){
-        return (((Holes *)[self.view viewWithTag:tag]).containsBall);
     }
-    else
-        return NO;
-    
+   // return  NO;
 }
 
+
+//-(void) handlePan:(UIPanGestureRecognizer *) sender
+//{
+//    
+//    //[sender.view bringSubviewToFront:self.view];
+//
+//    CGPoint translation  = [sender translationInView:sender.view];
+//    sender.view.center = CGPointMake(sender.view.center.x + translation.x, sender.view.center.y + translation.y);
+//    [sender setTranslation:CGPointMake(0,0) inView:sender.view];
+//    
+//}
+
+
+-(BOOL) checkingForMarble:(int)tag
+{
+      if([[[self.view viewWithTag:tag] subviews] count] == 0)
+      {
+          return NO;    // No marble
+      }
+      else
+      {
+          return YES;   // Contains marble
+      }
+}
+
+
+// Giving required tag depending on direction.
 -(int) requiredTag:(int)tag inDirection:(NSString *)direction
 {
-    
     if (direction == @"right")
         return tag + 1;
     else if (direction == @"left")
@@ -122,7 +225,7 @@
 
 -(BOOL) isValidHole:(int) tag
 {
-    return [holeBounds containsObject:[NSString stringWithFormat:@"%d",tag]];
+    return [holeNumber containsObject:[NSString stringWithFormat:@"%d",tag]];
 }
 
 - (void)didReceiveMemoryWarning
@@ -132,7 +235,7 @@
 }
 
 - (void)dealloc {
-    [holeBounds release];
+    [holeNumber release];
     [super dealloc];
 }
 @end
